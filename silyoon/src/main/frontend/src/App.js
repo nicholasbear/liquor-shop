9ad5855924a27c87e9cb233efa3b5d321d75629a
nicholasbear <nicholasbear@naver.com>
2023-07-12 14:06:02 +0900

    finish frontend

--- silyoon/src/main/frontend/src/App.js
+++ silyoon/src/main/frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LogInPage from "./LogInPage";
+import SignInPage from "./SignInPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <SignIn /> */}
+        <Route path="/signin" element={<SignInPage />} />
+        {/* <LogIn /> */}
+        <Route path="/" element={<LogInPage />} />
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
---
+++ silyoon/src/main/frontend/src/LogInPage.js
@@ -0,0 +1,65 @@
+import './LogInPage.css';
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useRef } from 'react';
+
+function LogInPage() {
+    const movePage = useNavigate();
+
+    function gosign(){
+    	movePage('/signin');
+    }
+    const [userIdInput, setUserIdInput] = useState('')
+    const [userPwInput, setUserPwInput] = useState('')
+    const [isShowPwChecked, setShowPwChecked] = useState(false)
+    const passwordRef = useRef(null)
+
+    const handleShowPwChecked = async () => {
+        const password = await passwordRef.current
+        if (password === null) return
+
+        await setShowPwChecked(!isShowPwChecked)
+        if(!isShowPwChecked) {
+            password.type = 'text';
+        } else {
+            password.type = 'password';
+        }
+    }
+
+    return (
+       
+    <div className="login_container">
+        <form>
+        <div >
+            <input className="input_row" type='text' name='userId' id='id' placeholder='아이디' 
+                    value={userIdInput}
+                    />
+            {
+            userIdInput && <button>X</button>
+            }
+        </div>
+        <div >
+            <input className="input_row" type='password' name='userPw' id='password' placeholder='비밀번호'
+                    maxLength={16}
+                    value={userPwInput}
+                    
+                    ref={passwordRef}/>
+            {
+            userPwInput && <button>X</button>
+            }
+        </div>
+        <div className="show_password_button">
+            <label>
+            <input type='checkbox' onChange={handleShowPwChecked} />
+            <span className="show_password_title">비밀번호 보기</span>
+            </label>
+        </div>
+        <div align="center" className="login_button">로그인</div>
+        <button align="center" className="join_button" onClick={gosign}>
+            회원가입
+        </button>
+        </form>
+    </div>
+    )
+}
+
+export default LogInPage;
